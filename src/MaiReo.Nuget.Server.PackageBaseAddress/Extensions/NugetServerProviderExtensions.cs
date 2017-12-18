50a8b8f8cb951c4dafdc464339eea77bec954af9
MaiReo <sawako.saki@gmail.com>
2017-12-18 14:21:33 +0800

    Implemented PackageBaseAddress ugly.

--- src/MaiReo.Nuget.Server.PackageBaseAddress/Extensions/NugetServerProviderExtensions.cs
+++ src/MaiReo.Nuget.Server.PackageBaseAddress/Extensions/NugetServerProviderExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using MaiReo.Nuget.Server.Core;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+using System;
 using System.ComponentModel;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace MaiReo.Nuget.Server.PackageBaseAddress
@@ -12,7 +16,26 @@ namespace MaiReo.Nuget.Server.PackageBaseAddress
             this INugetServerProvider provider,
             HttpContext context)
         {
+            var fsRoot = provider.NuspecProvider.GetPackageRootFullPath();
+            var fsRootUri = new Uri(fsRoot + "\\", UriKind.Absolute);
+            var requestFileUri = provider.GetRequestFileUri(context);
+            var fileUri = new Uri(fsRootUri, requestFileUri);
+            var fileInfo = new PhysicalFileInfo(new FileInfo(fileUri.LocalPath));
+            await context.Response.SendFileAsync(fileInfo, context.RequestAborted);
+        }
 
+        public static Uri GetRequestFileUri(
+            this INugetServerProvider provider,
+            HttpContext context)
+        {
+            var baseUrl = context.GetBaseUrl();
+            var pkgBaseAddr = provider.GetResourceUrlPath(
+                NugetServerResourceType.PackageBaseAddress);
+            var pkgBaseUri = new Uri(baseUrl + pkgBaseAddr + "/", UriKind.Absolute);
+
+            var pathUri = new Uri(baseUrl +
+                context.Request.Path.ToUriComponent(), UriKind.Absolute);
+            return pkgBaseUri.MakeRelativeUri(pathUri);
         }
 
         public static bool IsMatch(
